59c23cc82d8f8a00ac238a6f645dcbc284c16d87
yangxiaoqinng <421586027@qq.com>
2018-09-27 16:23:06 +0800

    测试2

--- src/main/java/com/lemon/pojo/User.java
+++ src/main/java/com/lemon/pojo/User.java
@@ -3,6 +3,6 @@ package com.lemon.pojo;
 public class User {
 
 	public static void main(String[] args) {
-		System.out.println("User2  xiaoqinng");
+		System.out.println("User23  xiaoqinng");
 	}
 }
